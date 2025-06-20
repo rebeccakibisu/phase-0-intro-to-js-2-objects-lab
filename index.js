e10b3fc25887eec9b21da035bee52babee273d2a
Rebecca Kibisu <coursecpd@gmail.com>
2025-06-21 00:16:31 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+const employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee= {...employee}
+    newEmployee[key]=value
+    return newEmployee
+}
+updateEmployeeWithKeyAndValue(employee, "name","Jared")
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.streetAddress= '12 Broadway'
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee2 = { ...employee }; // shallow clone
+    delete newEmployee2['name'];             // delete the key from the copy
+    return newEmployee2;                  // return the new object
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name
+    return employee
+}
